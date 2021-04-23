82c4a866d553e92d6234870f2f63afa372c4a4e2
Kolen <you@example.com>
2021-04-23 07:36:34 +0300

    fix

--- bot.py
+++ bot.py
@@ -214,7 +214,7 @@ async def help(ctx):
         embed.add_field(name="$help", value="Выводит данный список", inline=False)
         embed.add_field(name="$account", value="Выводит ифнормацию о вашем счете", inline=False)
         embed.add_field(name="$verify", value="Команда для получения верификационного кода", inline=False)
-        embed.add_field(name="$pay <Клиент> <Кол-во NextCoin-ов>", value="Команда для получения верефикационого кода", inline=False)
+        embed.add_field(name="$pay <Клиент> <Кол-во NextCoin-ов>", value="Команда для передачи клиенту NextBank NextCoin-ы", inline=False)
         embed.add_field(name="$give <Клиент> <Название предмета> <Кол-во предметов>", value="Команда для получения верефикационого кода", inline=False)
         embed.add_field(name="$inventory", value="Выводит информацию о ваших вещах в банке", inline=False)
     elif ctx.channel.id == 828200330969481248:
